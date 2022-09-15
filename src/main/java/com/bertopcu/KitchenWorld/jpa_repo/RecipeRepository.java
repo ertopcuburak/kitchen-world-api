f3a1d72609d02f29fb826882f0933454422c68d2
burak.ertopcu <burak.ertopcu@yandex.com>
2022-09-15 23:58:29 +0300

    having count > 1 update

--- src/main/java/com/bertopcu/KitchenWorld/jpa_repo/RecipeRepository.java
+++ src/main/java/com/bertopcu/KitchenWorld/jpa_repo/RecipeRepository.java
@@ -15,7 +15,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     @Query(value = "SELECT * FROM recipe WHERE name LIKE %?1% ORDER BY id DESC", nativeQuery = true)
     List<Recipe> findByRecipeName(String recipeName);
 
-    @Query(value = "SELECT DISTINCT recipe.* FROM recipe INNER JOIN recipe_material ON recipe_material.recipe_id = recipe.id LEFT JOIN material ON recipe_material.material_id = material.id WHERE material.id IN (?1) GROUP BY recipe.id HAVING COUNT(*) > 2 ORDER BY COUNT(*) DESC", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT recipe.* FROM recipe INNER JOIN recipe_material ON recipe_material.recipe_id = recipe.id LEFT JOIN material ON recipe_material.material_id = material.id WHERE material.id IN (?1) GROUP BY recipe.id HAVING COUNT(*) > 1 ORDER BY COUNT(*) DESC", nativeQuery = true)
     List<Recipe> findByMaterials(ArrayList<Integer> materialIds);
 
     @Query(value = "SELECT recipe.* FROM recipe INNER JOIN favorite ON favorite.recipe_id = recipe.id WHERE favorite.user_id = ?1 ORDER BY favorite.fav_date DESC", nativeQuery = true)

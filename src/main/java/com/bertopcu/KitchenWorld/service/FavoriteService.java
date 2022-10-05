936dcb16b5fd3df00265bb71ea8b2182d8b6afa4
burak.ertopcu <burak.ertopcu@yandex.com>
2022-10-05 23:21:05 +0300

    fav-serice logging update

--- src/main/java/com/bertopcu/KitchenWorld/service/FavoriteService.java
+++ src/main/java/com/bertopcu/KitchenWorld/service/FavoriteService.java
@@ -2,8 +2,12 @@ package com.bertopcu.KitchenWorld.service;
 
 import com.bertopcu.KitchenWorld.jpa_repo.FavoriteRepository;
 import com.bertopcu.KitchenWorld.model.Favorite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,12 +18,23 @@ import java.util.List;
 public class FavoriteService {
     @Autowired
     private FavoriteRepository favoriteRepository;
+
+    Logger logger = LoggerFactory.getLogger(FavoriteService.class);
     public List<Favorite> listAllFavorites() {
         return favoriteRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 
     public void saveFavorite(Favorite favorite) {
-        favoriteRepository.save(favorite);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        logger.debug("::saveFavorite starts:: ::loggedinUser:: {} ::param:: {}", currentPrincipalName, favorite.getRecipeId());
+        try {
+            favoriteRepository.save(favorite);
+            logger.debug("::saveFavorite success for:: {}", favorite.getRecipeId());
+        } catch (Exception e) {
+            logger.error("::error at saveFavorite::", e);
+            throw e;
+        }
     }
 
     public Favorite getFavorite(Integer id) {
@@ -27,7 +42,16 @@ public class FavoriteService {
     }
 
     public void deleteFavorite(Integer id) {
-        favoriteRepository.deleteById(id);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        logger.debug("::deleteFavorite starts:: ::loggedinUser:: {} ::param:: {}", currentPrincipalName, id);
+        try {
+            favoriteRepository.deleteById(id);
+            logger.debug("::deleteFavorite success for:: {}", id);
+        } catch (Exception e) {
+            logger.error("::error at deleteFavorite::", e);
+            throw e;
+        }
     }
 
     public List<Favorite> listAllFavsByUserId(Integer userId) {
@@ -35,6 +59,15 @@ public class FavoriteService {
     }
 
     public void deleteFavByUserAndRecipe(Integer userId, Integer recipeId) {
-        favoriteRepository.deleteByUserAndRecipe(userId, recipeId);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        logger.debug("::deleteFavByUserAndRecipe starts:: ::loggedinUser:: {} ::param-recipe:: {}", currentPrincipalName, recipeId);
+        try {
+            favoriteRepository.deleteByUserAndRecipe(userId, recipeId);
+            logger.debug("::deleteFavByUserAndRecipe success for:: {}", recipeId);
+        } catch (Exception e) {
+            logger.error("::error at deleteFavByUserAndRecipe::", e);
+            throw e;
+        }
     }
 }
